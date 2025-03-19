d9f0560459beb0b036dbfb76d3b0e96412682da8
kavita jakhar <117658345+kavijakhar@users.noreply.github.com>
2025-03-19 17:35:20 +0530

    Update superAdminController.js

--- controllers/superAdminController.js
+++ controllers/superAdminController.js
@@ -1,4 +1,4 @@
-const User = require("../models/User");
+const Admin = require("../models/Admin");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
@@ -6,15 +6,23 @@ exports.superAdminLogin = async (req, res) => {
   try {
     const { email, password } = req.body;
 
-    // Check if Super Admin exists
-    const superAdmin = await User.findOne({ email, role: "super_admin" });
+    // Find the admin by email and role "super_admin"
+    const superAdmin = await Admin.findOne({ email, role: "super_admin" });
 
     if (!superAdmin) {
       return res.status(404).json({ message: "Super Admin not found!" });
     }
-
-    // Compare password
+    async function hashPassword() {
+      const password = "securePassword123";  // Use the password you want to test
+      const hashedPassword = await bcrypt.hash(password, 10);
+      console.log("Hashed Password:", hashedPassword);
+    }
+    
+    hashPassword();
+    // Compare the provided password with the stored hash
     const isMatch = await bcrypt.compare(password, superAdmin.password);
+    console.log(isMatch)
+
     if (!isMatch) {
       return res.status(401).json({ message: "Invalid credentials!" });
     }
@@ -28,6 +36,7 @@ exports.superAdminLogin = async (req, res) => {
 
     res.status(200).json({ message: "Login successful!", token });
   } catch (error) {
+    console.error(error);
     res.status(500).json({ message: "Server error", error: error.message });
   }
 };
